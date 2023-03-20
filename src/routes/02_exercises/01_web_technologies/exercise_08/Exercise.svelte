c264aeb99833ab0cd44a748507593a16ba629259
HarveyMariya <harvey.okotie@student.uhasselt.be>
2023-03-20 15:35:12 +0100

    Exercises

---
+++ src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
@@ -0,0 +1,8 @@
+<script>
+const about_me = {
+    // name and things are the attributes
+    name: 'Harvey',
+    things: 'music, games, traveling'
+}
+</script>
+<p>My name is {about_me.name} and these are some of my favourite things:{about_me.things}</p>
